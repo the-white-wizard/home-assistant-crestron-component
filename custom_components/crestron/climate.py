97e395d70978371002d136ce17d5039e640f4404
the-white-wizard <bend3333@gmail.com>
2026-05-19 15:32:16 +0100

    Update climate.py
    Removed everything except heating functions - not necessary for my install, and this makes the thermostat cards in HA more simplified

--- custom_components/crestron/climate.py
+++ custom_components/crestron/climate.py
@@ -39,19 +39,19 @@ PLATFORM_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_NAME): cv.string,
         vol.Required(CONF_HEAT_SP_JOIN): cv.positive_int,
-        vol.Required(CONF_COOL_SP_JOIN): cv.positive_int,
+#        vol.Required(CONF_COOL_SP_JOIN): cv.positive_int,
         vol.Required(CONF_REG_TEMP_JOIN): cv.positive_int,
         vol.Required(CONF_MODE_HEAT_JOIN): cv.positive_int,
-        vol.Required(CONF_MODE_COOL_JOIN): cv.positive_int,
-        vol.Required(CONF_MODE_AUTO_JOIN): cv.positive_int,
-        vol.Required(CONF_MODE_OFF_JOIN): cv.positive_int,
-        vol.Required(CONF_FAN_ON_JOIN): cv.positive_int,
-        vol.Required(CONF_FAN_AUTO_JOIN): cv.positive_int,
+#        vol.Required(CONF_MODE_COOL_JOIN): cv.positive_int,
+#        vol.Required(CONF_MODE_AUTO_JOIN): cv.positive_int,
+#        vol.Required(CONF_MODE_OFF_JOIN): cv.positive_int,
+#        vol.Required(CONF_FAN_ON_JOIN): cv.positive_int,
+#        vol.Required(CONF_FAN_AUTO_JOIN): cv.positive_int,
         vol.Required(CONF_H1_JOIN): cv.positive_int,
-        vol.Optional(CONF_H2_JOIN): cv.positive_int,
-        vol.Required(CONF_C1_JOIN): cv.positive_int,
-        vol.Optional(CONF_C2_JOIN): cv.positive_int,
-        vol.Required(CONF_FA_JOIN): cv.positive_int,
+#        vol.Optional(CONF_H2_JOIN): cv.positive_int,
+#        vol.Required(CONF_C1_JOIN): cv.positive_int,
+#        vol.Optional(CONF_C2_JOIN): cv.positive_int,
+#        vol.Required(CONF_FA_JOIN): cv.positive_int,
     },
     extra=vol.ALLOW_EXTRA,
 )
@@ -66,33 +66,34 @@ class CrestronThermostat(ClimateEntity):
     def __init__(self, hub, config, unit):
         self._hub = hub
         self._hvac_modes = [
-            HVACMode.HEAT_COOL,
+#            HVACMode.HEAT_COOL,
             HVACMode.HEAT,
-            HVACMode.COOL,
-            HVACMode.OFF,
+#            HVACMode.COOL,
+#            HVACMode.OFF,
         ]
-        self._fan_modes = [FAN_ON, FAN_AUTO]
-        self._attr_supported_features = (
-            ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
-        )
+#        self._fan_modes = [FAN_ON, FAN_AUTO]
+#        self._attr_supported_features = (
+#            ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.TARGET_TEMPERATURE_RANGE
+#        )
+        self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
         self._should_poll = False
         self._temperature_unit = unit
 
         self._name = config[CONF_NAME]
         self._heat_sp_join = config[CONF_HEAT_SP_JOIN]
-        self._cool_sp_join = config[CONF_COOL_SP_JOIN]
+#        self._cool_sp_join = config[CONF_COOL_SP_JOIN]
         self._reg_temp_join = config[CONF_REG_TEMP_JOIN]
         self._mode_heat_join = config[CONF_MODE_HEAT_JOIN]
-        self._mode_cool_join = config[CONF_MODE_COOL_JOIN]
-        self._mode_auto_join = config[CONF_MODE_AUTO_JOIN]
-        self._mode_off_join = config[CONF_MODE_OFF_JOIN]
-        self._fan_on_join = config[CONF_FAN_ON_JOIN]
-        self._fan_auto_join = config[CONF_FAN_AUTO_JOIN]
+#        self._mode_cool_join = config[CONF_MODE_COOL_JOIN]
+#        self._mode_auto_join = config[CONF_MODE_AUTO_JOIN]
+#        self._mode_off_join = config[CONF_MODE_OFF_JOIN]
+#        self._fan_on_join = config[CONF_FAN_ON_JOIN]
+#        self._fan_auto_join = config[CONF_FAN_AUTO_JOIN]
         self._h1_join = config[CONF_H1_JOIN]
-        self._h2_join = config.get(CONF_H2_JOIN)
-        self._c1_join = config[CONF_C1_JOIN]
-        self._c2_join = config.get(CONF_C2_JOIN)
-        self._fa_join = config[CONF_FA_JOIN]
+#        self._h2_join = config.get(CONF_H2_JOIN)
+#        self._c1_join = config[CONF_C1_JOIN]
+#        self._c2_join = config.get(CONF_C2_JOIN)
+#        self._fa_join = config[CONF_FA_JOIN]
         self._unique_id = slugify(f"{DOMAIN}_climate_{self._name}")
 
     async def async_added_to_hass(self):
@@ -116,9 +117,9 @@ class CrestronThermostat(ClimateEntity):
     def hvac_modes(self):
         return self._hvac_modes
 
-    @property
-    def fan_modes(self):
-        return self._fan_modes
+#    @property
+#    def fan_modes(self):
+#        return self._fan_modes
 
     @property
     def supported_features(self):
@@ -136,38 +137,42 @@ class CrestronThermostat(ClimateEntity):
     def current_temperature(self):
         return self._hub.get_analog(self._reg_temp_join) / 10
 
-    @property
-    def target_temperature_high(self):
-        return self._hub.get_analog(self._cool_sp_join) / 10
+#    @property
+#    def target_temperature_high(self):
+#        return self._hub.get_analog(self._cool_sp_join) / 10
+
+#    @property
+#    def target_temperature_low(self):
+#        return self._hub.get_analog(self._heat_sp_join) / 10
 
     @property
-    def target_temperature_low(self):
+    def target_temperature(self):
         return self._hub.get_analog(self._heat_sp_join) / 10
 
     @property
     def hvac_mode(self):
-        if self._hub.get_digital(self._mode_auto_join):
-            return HVACMode.HEAT_COOL
-        if self._hub.get_digital(self._mode_heat_join):
+#        if self._hub.get_digital(self._mode_auto_join):
+#            return HVACMode.HEAT_COOL
+#        if self._hub.get_digital(self._mode_heat_join):
             return HVACMode.HEAT
-        if self._hub.get_digital(self._mode_cool_join):
-            return HVACMode.COOL
-        if self._hub.get_digital(self._mode_off_join):
-            return HVACMode.OFF
+#        if self._hub.get_digital(self._mode_cool_join):
+#            return HVACMode.COOL
+#        if self._hub.get_digital(self._mode_off_join):
+#            return HVACMode.OFF
 
-    @property
-    def fan_mode(self):
-        if self._hub.get_digital(self._fan_auto_join):
-            return FAN_AUTO
-        if self._hub.get_digital(self._fan_on_join):
-            return FAN_ON
+#    @property
+#    def fan_mode(self):
+#        if self._hub.get_digital(self._fan_auto_join):
+#            return FAN_AUTO
+#        if self._hub.get_digital(self._fan_on_join):
+#            return FAN_ON
 
     @property
     def hvac_action(self):
-        if self._hub.get_digital(self._h1_join) or self._hub.get_digital(self._h2_join):
+        if self._hub.get_digital(self._h1_join): # or self._hub.get_digital(self._h2_join):
             return HVACAction.HEATING
-        elif self._hub.get_digital(self._c1_join) or self._hub.get_digital(self._c2_join):
-            return HVACAction.COOLING
+#        elif self._hub.get_digital(self._c1_join) or self._hub.get_digital(self._c2_join):
+#            return HVACAction.COOLING
         else:
             return HVACAction.IDLE
 
@@ -176,35 +181,38 @@ class CrestronThermostat(ClimateEntity):
         return self._unique_id
 
     async def async_set_hvac_mode(self, hvac_mode):
-        if hvac_mode == HVACMode.HEAT_COOL:
-            self._hub.set_digital(self._mode_cool_join, False)
-            self._hub.set_digital(self._mode_off_join, False)
-            self._hub.set_digital(self._mode_heat_join, False)
-            self._hub.set_digital(self._mode_auto_join, True)
+#        if hvac_mode == HVACMode.HEAT_COOL:
+#            self._hub.set_digital(self._mode_cool_join, False)
+#            self._hub.set_digital(self._mode_off_join, False)
+#            self._hub.set_digital(self._mode_heat_join, False)
+#            self._hub.set_digital(self._mode_auto_join, True)
         if hvac_mode == HVACMode.HEAT:
             self._hub.set_digital(self._mode_auto_join, False)
             self._hub.set_digital(self._mode_cool_join, False)
             self._hub.set_digital(self._mode_off_join, False)
             self._hub.set_digital(self._mode_heat_join, True)
-        if hvac_mode == HVACMode.COOL:
-            self._hub.set_digital(self._mode_auto_join, False)
-            self._hub.set_digital(self._mode_off_join, False)
-            self._hub.set_digital(self._mode_heat_join, False)
-            self._hub.set_digital(self._mode_cool_join, True)
+#        if hvac_mode == HVACMode.COOL:
+#            self._hub.set_digital(self._mode_auto_join, False)
+#            self._hub.set_digital(self._mode_off_join, False)
+#            self._hub.set_digital(self._mode_heat_join, False)
+#            self._hub.set_digital(self._mode_cool_join, True)
         if hvac_mode == HVACMode.OFF:
             self._hub.set_digital(self._mode_auto_join, False)
-            self._hub.set_digital(self._mode_cool_join, False)
+#            self._hub.set_digital(self._mode_cool_join, False)
             self._hub.set_digital(self._mode_heat_join, False)
             self._hub.set_digital(self._mode_off_join, True)
 
-    async def async_set_fan_mode(self, fan_mode):
-        if fan_mode == FAN_AUTO:
-            self._hub.set_digital(self._fan_on_join, False)
-            self._hub.set_digital(self._fan_auto_join, True)
-        if fan_mode == FAN_ON:
-            self._hub.set_digital(self._fan_auto_join, False)
-            self._hub.set_digital(self._fan_on_join, True)
+#    async def async_set_fan_mode(self, fan_mode):
+#        if fan_mode == FAN_AUTO:
+#            self._hub.set_digital(self._fan_on_join, False)
+#            self._hub.set_digital(self._fan_auto_join, True)
+#        if fan_mode == FAN_ON:
+#            self._hub.set_digital(self._fan_auto_join, False)
+#            self._hub.set_digital(self._fan_on_join, True)
 
+#    async def async_set_temperature(self, **kwargs):
+#        self._hub.set_analog(self._heat_sp_join, int(kwargs["target_temp_low"]) * 10)
+#        self._hub.set_analog(self._cool_sp_join, int(kwargs["target_temp_high"]) * 10)
+        
     async def async_set_temperature(self, **kwargs):
-        self._hub.set_analog(self._heat_sp_join, int(kwargs["target_temp_low"]) * 10)
-        self._hub.set_analog(self._cool_sp_join, int(kwargs["target_temp_high"]) * 10)
+        self._hub.set_analog(self._heat_sp_join, int(kwargs["temperature"]) * 10)
